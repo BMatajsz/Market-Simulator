5c2cfdaef80f89052bc1eaf57ab2bb2430a01787
BMatajsz <90217645+BMatajsz@users.noreply.github.com>
2024-02-19 20:53:27 +0100

    Added SQLite basics

--- src/scraper/scraper.js
+++ src/scraper/scraper.js
@@ -2,6 +2,8 @@ const { getRandomTime, getRandomAgent } = require("./utils.js");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const { get } = require("http");
+let stocks = [];
+
 async function scrapeStocks() {
     try {
         const response = await axios.get("https://finance.yahoo.com/most-active/", {
@@ -11,7 +13,6 @@ async function scrapeStocks() {
         });
 
         const $ = cheerio.load(response.data);
-        const stocks = [];
         $("table tbody tr").each((index, element) => {
             if (index < 10) {
                 const symbol = $(element).find("td:nth-child(1) a").text();
@@ -22,7 +23,12 @@ async function scrapeStocks() {
                 stocks.push({ symbol, name, price, change, changePerc });
             }
         });
+
+        const delay = getRandomTime(2000, 12000); //gets random time berween 2 and 5 seconds
+        await Promise((resolve) => setTimeout(resolve, delay)); //resolves promise in delayed seconds
     } catch (error) {
         console.error("Error:", error.message);
     }
 }
+
+module.exports = { stocks };
